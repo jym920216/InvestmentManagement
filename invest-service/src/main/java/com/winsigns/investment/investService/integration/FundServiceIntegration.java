bb57f227d4bfce0bad8f6309e40be3e62504dc98
yiming.jin <yiming.jin@winsigns.com.cn>
2017-04-05 18:37:34 +0800

    修改

--- invest-service/src/main/java/com/winsigns/investment/investService/integration/FundServiceIntegration.java
+++ invest-service/src/main/java/com/winsigns/investment/investService/integration/FundServiceIntegration.java
@@ -3,7 +3,6 @@ package com.winsigns.investment.investService.integration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import com.jayway.jsonpath.JsonPath;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.winsigns.investment.framework.integration.AbstractIntegration;
 
@@ -27,7 +26,8 @@ public class FundServiceIntegration extends AbstractIntegration {
   public Long getPortfolioInvestManager(Long portfolioId) {
     ResponseEntity<String> resultStr = restTemplate.getForEntity(
         this.getIntegrationURI() + String.format(portfolioURL, portfolioId), String.class);
-    return JsonPath.read(resultStr.getBody(), "$.investManagerId");
+    // return JsonPath.read(resultStr.getBody(), "$.investManagerId");
+    return 1L;
   }
 
   public Long defaultPortfolioInvestManager(Long portfolioId) {
